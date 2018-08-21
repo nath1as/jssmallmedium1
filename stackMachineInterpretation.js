098b569d2ab33d7a0e13c9b38336c9ae6977aba0
nathias <n@th1.as>
2018-08-21 13:27:47 +0200

    stack machine

---
+++ stackMachineInterpretation.js
@@ -0,0 +1,68 @@
+function minilang(program) {
+  var register = 0;
+  var stack = [];
+
+  program.split(' ').forEach(function (token) {
+    switch (token) {
+    case 'ADD':
+      register += stack.pop();
+      break;
+    case 'DIV':
+      register = Math.floor(register / stack.pop());
+      break;
+    case 'MOD':
+      register = Math.floor(register % stack.pop());
+      break;
+    case 'MULT':
+      register *= stack.pop();
+      break;
+    case 'SUB':
+      register -= stack.pop();
+      break;
+    case 'PUSH':
+      stack.push(register);
+      break;
+    case 'POP':
+      register = stack.pop();
+      break;
+    case 'PRINT':
+      console.log(register);
+      break;
+    default:
+      register = parseInt(token, 10);
+    }
+  });
+}
+
+
+minilang('PRINT');
+// 0
+
+minilang('5 PUSH 3 MULT PRINT');
+// 15
+
+minilang('5 PRINT PUSH 3 PRINT ADD PRINT');
+// 5
+// 3
+// 8
+
+minilang('5 PUSH POP PRINT');
+// 5
+
+minilang('3 PUSH 4 PUSH 5 PUSH PRINT ADD PRINT POP PRINT ADD PRINT');
+// 5
+// 10
+// 4
+// 7
+
+minilang('3 PUSH PUSH 7 DIV MULT PRINT');
+// 6
+
+minilang('4 PUSH PUSH 7 MOD MULT PRINT');
+// 12
+
+minilang('-3 PUSH 5 SUB PRINT');
+// 8
+
+minilang('6 PUSH');
+// (nothing is printed because the `program` argument has no `PRINT` commands)
