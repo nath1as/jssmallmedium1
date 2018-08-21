94c8a1a98478a77ef1597dd1a63b16649e72595e
nathias <n@th1.as>
2018-08-21 15:24:41 +0200

    fibonacci recursive

---
+++ fibonacciNumbersRecursion.js
@@ -0,0 +1,17 @@
+function fibonacci(n) {
+  if (n <= 2) {
+    return 1;
+  } else {
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  }
+}
+
+console.log(
+  fibonacci(1),       // 1
+  fibonacci(2),       // 1
+  fibonacci(3),       // 2
+  fibonacci(4),       // 3
+  fibonacci(5),       // 5
+  fibonacci(12),      // 144
+  fibonacci(20),      // 6765
+);
